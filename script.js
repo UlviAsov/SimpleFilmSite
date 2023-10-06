33b838e85ffe253f92735ff0d7ed320466d6d69d
Aulv1 <ulvi3457abbasli@gmail.com>
2023-10-07 02:54:26 +0400

    axtaris zamani enterden istifade elave edildi

--- script.js
+++ script.js
@@ -52,3 +52,60 @@ $('.search-btn').on('click', ()=>{
     console.log(res)
     })
 })
+
+window.onkeydown = (e) =>{
+    if(e.which == "13"){
+            $('#film').css({
+        display:'flex'
+    })
+    film = inputValue.value
+    inputValue.value = ''
+    $.ajax(`http://www.omdbapi.com/?apikey=164ceb3c&t=${film}`).then(function(res){
+    $('.filmImg').attr('src',res.Poster);
+    $('.imdb').text(res.imdbRating);
+    $('.title').text(res.Title);
+    $('.year').text(res.Year);
+    $('.content').text(res.Plot);
+    $('.actors').text(res.Actors);
+    if(res.Director == "N/A"){
+        $('.director-fluid').css({
+            display:"none"
+        })
+    }
+    else{
+        $('.director').text(res.Director);
+    }
+    $('.released').text(res.Released);
+    $('.country').text(res.Country);
+    $('.genre').text(res.Genre);
+    if(res.Type == "series"){
+        $('.runtimeAndSeasons').css({
+            display:"flex"
+        })
+        $('.runtime-fluid').css({
+            display:"none"
+        })
+    }
+    else{
+        $('.runtime-fluid').css({
+            display:"block"
+        })
+        $('.runtimeAndSeasons').css({
+            display:"none"
+        })
+    }
+    if(res.Awards == "N/A"){
+        $('.awards-fluid').css({
+            display:"none"
+        })
+    }
+    else{
+        $('.awards').text(res.Awards);
+    }
+    $('.runtime').text(res.Runtime)
+    $('.totalSeasons').text(res.totalSeasons)
+    console.log(res)
+    })
+    }
+
+}
